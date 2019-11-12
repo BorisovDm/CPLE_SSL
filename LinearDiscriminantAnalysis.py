56c6ca527ff509d52e9671ef943c37026e5003f7
Dmitrii Borisov <abdima2010@mail.ru>
2019-11-12 22:02:59 +0100

    implement LinearDiscriminantAnalysis

---
+++ LinearDiscriminantAnalysis.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import linalg
+from utils import class_cov, class_means, softmax
+
+
+class LinearDiscriminantAnalysis:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.classes, y_t = np.unique(y, return_inverse=True)
+        self._priors = np.bincount(y_t) / len(y)
+        self._means = class_means(X, y)
+        self._covariance = class_cov(X, y, self._priors)
+        self._coef = linalg.lstsq(self._covariance, self._means.T)[0]
+        self._intercept = -0.5 * np.diag(np.dot(self._means, self._coef)) + np.log(self._priors)
+
+        return self
+
+    def predict_proba(self, X):
+        scores = np.dot(X, self._coef) + self._intercept
+        return softmax(scores)
+
+    def predict(self, X):
+        scores = np.dot(X, self._coef) + self._intercept
+        return self.classes[np.argmax(scores, axis=1)]
